747b01e19f024e49038302869429d7ef02ce8b00
jdc1731 <jkeil@byupathway.edu>
2025-05-14 18:00:00 -0600

    save

--- scripts/index.js
+++ scripts/index.js
@@ -22,42 +22,84 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const courses = [
   {
-    courseName: "Introduction to Programming ",
-    courseCode: "CSE 110",
-    completed: true,
+    subject: 'CSE',
+    number: 110,
+    title: 'Introduction to Programming',
     credits: 2,
+    certificate: 'Web and Computer Programming',
+    description: 'This course will introduce students to programming. It will introduce the building blocks of programming languages (variables, decisions, calculations, loops, array, and input/output) and use them to solve problems.',
+    technology: [
+      'Python'
+    ],
+    completed: false
   },
   {
-    courseName: "Programming with Functions",
-    courseCode: "CSE 111 ",
-    completed: true,
+    subject: 'WDD',
+    number: 130,
+    title: 'Web Fundamentals',
     credits: 2,
+    certificate: 'Web and Computer Programming',
+    description: 'This course introduces students to the World Wide Web and to careers in web site design and development. The course is hands on with students actually participating in simple web designs and programming. It is anticipated that students who complete this course will understand the fields of web design and development and will have a good idea if they want to pursue this degree as a major.',
+    technology: [
+      'HTML',
+      'CSS'
+    ],
+    completed: false
   },
   {
-    courseName: "Programming with Classes",
-    courseCode: "CSE 210",
-    completed: false,
+    subject: 'CSE',
+    number: 111,
+    title: 'Programming with Functions',
     credits: 2,
+    certificate: 'Web and Computer Programming',
+    description: 'CSE 111 students become more organized, efficient, and powerful computer programmers by learning to research and call functions written by others; to write, call , debug, and test their own functions; and to handle errors within functions. CSE 111 students write programs with functions to solve problems in many disciplines, including business, physical science, human performance, and humanities.',
+    technology: [
+      'Python'
+    ],
+    completed: false
   },
   {
-    courseName: "Web Fundamentals",
-    courseCode: "WDD 130",
-    completed: true,
+    subject: 'CSE',
+    number: 210,
+    title: 'Programming with Classes',
     credits: 2,
+    certificate: 'Web and Computer Programming',
+    description: 'This course will introduce the notion of classes and objects. It will present encapsulation at a conceptual level. It will also work with inheritance and polymorphism.',
+    technology: [
+      'C#'
+    ],
+    completed: false
   },
   {
-    courseName: "Dynamic Web Fundamentals",
-    courseCode: "WDD 131",
-    completed: true,
+    subject: 'WDD',
+    number: 131,
+    title: 'Dynamic Web Fundamentals',
     credits: 2,
+    certificate: 'Web and Computer Programming',
+    description: 'This course builds on prior experience in Web Fundamentals and programming. Students will learn to create dynamic websites that use JavaScript to respond to events, update content, and create responsive user experiences.',
+    technology: [
+      'HTML',
+      'CSS',
+      'JavaScript'
+    ],
+    completed: false
   },
   {
-    courseName: "Web Frontend Development 1",
-    courseCode: "WDD 231",
-    completed: false,
+    subject: 'WDD',
+    number: 231,
+    title: 'Frontend Web Development I',
     credits: 2,
-  },
+    certificate: 'Web and Computer Programming',
+    description: 'This course builds on prior experience with Dynamic Web Fundamentals and programming. Students will focus on user experience, accessibility, compliance, performance optimization, and basic API usage.',
+    technology: [
+      'HTML',
+      'CSS',
+      'JavaScript'
+    ],
+    completed: false
+  }
 ];
+
 
 const calculateCreditsCompleted = () => {
   const completedCredits = courses
@@ -74,7 +116,7 @@ const filterCourses = (type) => {
   if (type === "All") {
     filteredCourses = courses;
   } else {
-    filteredCourses = courses.filter(course => course.courseCode.includes(type));
+    filteredCourses = courses.filter(course => course.subject.includes(type));
   }
 
   const courseList = document.getElementById("course-list");
@@ -82,7 +124,7 @@ const filterCourses = (type) => {
 
   filteredCourses.forEach(course => {
     const courseDiv = document.createElement("div");
-    courseDiv.textContent = course.courseCode;
+    courseDiv.textContent = `${course.subject} ${course.number}`;
     courseDiv.classList.add("course-item");
 
     if (course.completed) {
