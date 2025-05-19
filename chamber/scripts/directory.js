4f7a3ffbd2bd775b164a187f8e9545a58972feb8
jdc1731 <jkeil@byupathway.edu>
2025-05-19 16:43:20 -0600

    almost there

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,7 +1,58 @@
+document.addEventListener('DOMContentLoaded', () => {
+    //  Update current year and last modified
+    const currentYear = new Date().getFullYear();
+    document.querySelector('#current-year').textContent = `${currentYear}`;
+    const lastModifiedDate = document.lastModified;
+    document.querySelector('#last-modified').textContent = `Date Last Modified ${lastModifiedDate}`;
+    
 const hamburgerElement = document.querySelector('#myButton');
 const navMenu = document.querySelector('.menuLinks');
 
 hamburgerElement.addEventListener('click', () => {
     navMenu.classList.toggle('open');
-    // hamburgerElement.classList.toggle('open');
+    hamburgerElement.classList.toggle('open');
 });
+});
+
+
+const url = 'data/members.json';
+const cardsContainer = document.getElementById('members');
+const gridButton = document.getElementById('grid-view');
+const listButton = document.getElementById('list-view');
+
+async function getMembers() {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        displayMembers(data.members);
+    } catch (error) {
+        console.error('Error fetching members:', error);
+    }
+}
+function displayMembers(members) {
+    cardsContainer.innerHTML = '';
+    members.forEach(member => {
+        const card = document.createElement('section');
+        card.classList.add('card');
+        card.innerHTML = `
+            <h2>${member.name}</h2>
+            <p>${member.phone}</p>
+            <p>${member.address}</p>
+            <a href="${member.website}" target="_blank">Website</a>
+            <img src="${member.image}" alt="${member.name}">
+        `;
+        cardsContainer.appendChild(card);
+     
+    });
+}
+
+gridButton.addEventListener('click', () => {
+    cardsContainer.classList.add('grid');
+    cardsContainer.classList.remove('list');
+});
+
+listButton.addEventListener('click', () => {
+    cardsContainer.classList.add('list');
+    cardsContainer.classList.remove('grid');
+});
+getMembers();
