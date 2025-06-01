38ddcef53e088065c4ea7440ae2560145713baba
jdc1731 <jkeil@byupathway.edu>
2025-06-01 09:51:29 -0600

    what is wrong with this

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -1,7 +1,7 @@
 import { updateFooterDates, setupHamburgerMenu } from "./header-footer.js";
 import { getEvents } from "./current-events.js";
 import { fetchWeather } from "./weather.js";
-import { displayMembers, displaySpotlights, getMembers } from "./members.js";
+import { displaySpotlights, getMembers } from "./members.js";
 
 
 document.addEventListener("DOMContentLoaded", () => {
